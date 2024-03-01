11e2bb6541e6fd30a1a8570116b62e7c80ec6a05
root <1063334866@qq.com>
2024-03-01 21:42:26 +0800

    sasa

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -23,6 +23,7 @@ public class HmDianPingApplication {
         System.out.println("caonima");
         System.out.println("sa");
         System.out.println("8");
+        System.out.println();
         SpringApplication.run(HmDianPingApplication.class, args);
     }
 
