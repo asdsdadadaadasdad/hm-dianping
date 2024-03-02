6dbb06b3840b2ea2a23a3a75b3d01d1857905fd2
root <1063334866@qq.com>
2024-03-03 00:07:41 +0800

    sasa

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -30,6 +30,9 @@ public class HmDianPingApplication {
         System.out.println("sdsidie");
         SpringApplication.run(HmDianPingApplication.class, args);
         System.out.println("ma");
-    }
 
+    }
+    void kkk(){
+
+    }
 }
